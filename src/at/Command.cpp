393e32cd3d143af2a04b5fabafaef51164165d37
alexander.rozov <alexander.rozov@gg.com>
2026-09-09 15:46:20 +0400

    remove AT

--- src/at/Command.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by fog54 on 09.09.2026.
-//
-
-#include "../../include/at/Command.h"
-
-
-namespace at {}
--- src/modem/ModemState.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by fog54 on 09.09.2026.
-//
-
-//#include "../include/modem/ModemState.h/"
